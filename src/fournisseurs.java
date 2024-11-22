025692a7868b0402abce82342daae4a497ae109d
Babouille404 <baolongle.pro@gmail.com>
2024-11-22 09:24:21 +0100

    v2 du bouton modifier, j'avais pas mit mon travail !

--- src/fournisseurs.java
+++ src/fournisseurs.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.nio.channels.ClosedSelectorException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -32,6 +33,8 @@ public class fournisseurs extends JFrame {
         System.out.println("Voici les fournisseurs : " + liste);
     }
 
+
+    //Ajouter
     public void Ajouter(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Ajouter le fournisseur : " );
@@ -40,20 +43,26 @@ public class fournisseurs extends JFrame {
 
 
 
+        //Si la personne n'écrit rien, ça renvoit à un question !
        if (input.length() == 0){
            System.out.println("Bah t'as rien écrit ?");
            Scanner sc1 = new Scanner(System.in);
            System.out.println("Est ce que tu voulais vraiment rajouter un fournisseur ?");
            input = sc1.nextLine().toLowerCase();
+
+           //Si il veut vraiment faut répondre yes
            if (input.equalsIgnoreCase("yes")){
                Ajouter();
-           } else if (input.equalsIgnoreCase("no")) {
+           }
+           //Sinon il mets no et ça annule tout
+           else if (input.equalsIgnoreCase("no")) {
                System.out.println("Bon bah tant pis");
 
            }
        }
     }
 
+    //Pour supprimer un objet de la liste
     public void Supprimer() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Lequel vous voulez supprimer ? ");
@@ -62,19 +71,41 @@ public class fournisseurs extends JFrame {
 
     }
 
+    //Pour modifier un objet de la liste
     public void Modifier(){
         Scanner sc = new Scanner(System.in);
-        System.out.println(liste);
+        System.out.println(liste); //Pour pouvoir avoir les noms des fournisseurs avant de savoir lequel modifier
         System.out.println("Ecrire celui à modifier");
         String input = sc.nextLine().toLowerCase();
+        System.out.println("Tu es bien sûr de vouloir modifier celui là ? ");
+        input = sc.nextLine().toLowerCase();
+
+        if (input.equalsIgnoreCase("yes")){
+            System.out.println("Vas-y fais la modification !");
+            int valeur = liste.indexOf(input);
+            if (valeur != -1) {
+                System.out.print("Entrez la nouvelle valeur : ");
+                String modif = sc.nextLine();
+
+                liste.set(valeur, modif);
+                System.out.println("La liste modifiée : " + liste);
+            } else {
+                System.out.println("L'élément n'a pas été trouvé dans la liste.");
+            }
+            System.out.println("La modification a bien été prise en compte ! ");
+        } else if (input.equalsIgnoreCase("no")) {
+            System.out.println("Bon bah retour à la case départ :) ");
+
+        }
 
 
     }
 
     public static void main(String[] args) {
         fournisseurs f = new fournisseurs();
+
         f.Ajouter();
-        f.Consulter();
+        f.Ajouter();
         f.Ajouter();
         f.Consulter();
         f.Modifier();
