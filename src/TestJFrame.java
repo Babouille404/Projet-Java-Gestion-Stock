8f49bf30cf071b2b539d556c84910f2a5c6fe4bf
Anissa Dahabi <anissa.Dahabi@gmail.com>
2025-01-08 21:31:09 +0100

    Fusion de la classe BddConnection avec TestJFrame

--- src/TestJFrame.java
+++ src/TestJFrame.java
@@ -1,9 +1,23 @@
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 //Classe pour permettre à l'interface de run!!
 
 public class TestJFrame {
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/projetjava", "root", "root");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("show databases;");
+            System.out.println("Connected");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         SwingUtilities.invokeLater(new Runnable() {
             public void run (){
 
