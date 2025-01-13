d6d6b5eeb638c3d5af28412cd641e9893266c155
Babouille404 <baolongle.pro@gmail.com>
2025-01-13 22:55:48 +0100

    Classe Fournisseurs.java màj et FournisseursQuery.java màj, manque quelques trucs mais c'est pour vous donner une vision ! Bisou

--- src/fr/abl/gestionjava/dao/FournisseursQuery.java
+++ src/fr/abl/gestionjava/dao/FournisseursQuery.java
@@ -1,4 +1,97 @@
 package fr.abl.gestionjava.dao;
 
-public class FournisseursQuery {
+import fr.abl.gestionjava.model.Fournisseurs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class FournisseursQuery extends Component {
+
+    private JTextField idField;
+    private JTextField nameField;
+
+public void addFournisseur(Fournisseurs fournisseur) {
+    try {
+// Connexion à la base de données
+        Connection con =
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/projetjava", "root",
+                        "user");
+
+// Préparation de la requête SQL
+        String query = "INSERT INTO fournisseur (Id_Fournisseur, Nom_Fournisseur, Mail_Fournisseur, Addresse_Fournisseur, Num_Fournisseur) VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement pstmt = con.prepareStatement(query);
+// Récupération des données saisies par l'utilisateur
+        int id = Integer.parseInt(idField.getText());
+        String nameFournisseur = nameField.getText();
+        String mailFournisseur = nameField.getText();
+        String addressFournisseur = nameField.getText();
+        String numFournisseur = nameField.getText();
+// Remplissage des paramètres de la requête SQL
+        pstmt.setInt(1, id);
+        pstmt.setString(2, nameFournisseur);
+        pstmt.setString(3, mailFournisseur);
+        pstmt.setString(4, addressFournisseur);
+        pstmt.setString(5, numFournisseur);
+
+// Exécution de la requête SQL
+        int rowsAffected = pstmt.executeUpdate();
+// Fermeture de la connexion et du PreparedStatement
+        pstmt.close();
+        con.close();
+// Affichage d'un message de succès
+        JOptionPane.showMessageDialog(this, "Fournisseur ajouté avec succès!");
+    } catch (SQLException ex) {
+        ex.printStackTrace();
+        JOptionPane.showMessageDialog(this, "Erreur lors de l'ajout du fournisseur: " + ex.getMessage());
+    }
 }
+
+    public void delFournisseur(Fournisseurs fournisseur) {
+        try {
+            // Connexion à la base de données
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/swing", "root", "user");
+
+            // Préparation de la requête SQL
+            String query = "DELETE FROM fournisseur WHERE Id_Fournisseur = ?";
+            PreparedStatement pstmt = con.prepareStatement(query);
+
+            // Récupération de l'ID saisi par l'utilisateur
+            int id = Integer.parseInt(idField.getText());
+
+            // Remplissage du paramètre de la requête SQL
+            pstmt.setInt(1, id);
+
+            // Exécution de la requête SQL
+            int rowsAffected = pstmt.executeUpdate();
+
+            // Fermeture des ressources
+            pstmt.close();
+            con.close();
+
+            if (rowsAffected > 0) {
+                JOptionPane.showMessageDialog(this, "Fournisseur supprimé avec succès !");
+            } else {
+                JOptionPane.showMessageDialog(this, "Aucun fournisseur trouvé avec cet ID.");
+            }
+        } catch (SQLException ex) {
+            // Gestion des erreurs SQL
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Erreur lors de la suppression du fournisseur : " + ex.getMessage());
+        } catch (NumberFormatException ex) {
+            // Gestion des erreurs de conversion
+            JOptionPane.showMessageDialog(this, "Veuillez entrer un ID fournisseur valide.");
+        }
+    }
+
+
+
+}
+
+
+
+
--- src/fr/abl/gestionjava/model/Fournisseurs.java
+++ src/fr/abl/gestionjava/model/Fournisseurs.java
@@ -1,4 +1,58 @@
 package fr.abl.gestionjava.model;
 
 public class Fournisseurs {
+
+private int IdFournisseur;
+private String NomFournisseur;
+private String AdresseFournisseur;
+private String EmailFournisseur;
+private String TelephoneFournisseur;
+
+public Fournisseurs(int id, String nom) {
+    this.IdFournisseur = id;
+    this.NomFournisseur = nom;
+    this.AdresseFournisseur = "";
+    this.EmailFournisseur = "";
+    this.TelephoneFournisseur = "";
+
+
+}
+public int getIdFournisseur() {
+    return IdFournisseur;
+}
+public void setIdFournisseur(int IdFournisseur) {
+    this.IdFournisseur = IdFournisseur;
+}
+public String getNomFournisseur() {
+    return NomFournisseur;
+}
+public void setNomFournisseur(String NomFournisseur) {
+    this.NomFournisseur = NomFournisseur;
+}
+public String getAdresseFournisseur() {
+    return AdresseFournisseur;
+}
+public void setAdresseFournisseur(String AdresseFournisseur) {
+    this.AdresseFournisseur = AdresseFournisseur;
+}
+public String getEmailFournisseur() {
+    return EmailFournisseur;
+}
+public void setEmailFournisseur(String EmailFournisseur) {
+    this.EmailFournisseur = EmailFournisseur;
+}
+public String getTelephoneFournisseur() {
+    return TelephoneFournisseur;
+}
+public void setTelephoneFournisseur(String TelephoneFournisseur) {
+    this.TelephoneFournisseur = TelephoneFournisseur;
+}
+
+//Pour contrôler les sorties SQL si JFrame fonctionne pas !!
+@Override
+    public String toString() {
+    return "Ton fournisseur est : " + this.IdFournisseur + " son nom est : " + this.NomFournisseur
+            + " et ses infos sont : " + this.EmailFournisseur + "," + this.AdresseFournisseur + "," + this.TelephoneFournisseur;
+}
+
 }
