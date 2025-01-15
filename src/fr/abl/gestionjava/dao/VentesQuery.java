40e905bf2608d2d56a355e2f3a5eb74affc2071d
palpaMX <lois.guilloux77@gmail.com>
2025-01-15 18:36:17 +0100

    Correction de l'erreur.

--- src/fr/abl/gestionjava/dao/VentesQuery.java
+++ src/fr/abl/gestionjava/dao/VentesQuery.java
@@ -1,4 +1,49 @@
 package fr.abl.gestionjava.dao;
 
-public class VentesQuery {
-}
+import javax.swing.*;
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class VentesQuery extends Component {
+        private JTextField idfield;
+        private JTextField namefield;
+
+                public void addVentes() {
+                    try {
+    //connection a la base de données sql
+                        Connection con =
+                                DriverManager.getConnection("jdbc:mysql://localhost:3306/projetjava", "root", "user");
+
+    //requete sql
+                        String query = "insert into ventes (id-ventes, name_ventes) VALUES(?, ?)";
+                        PreparedStatement pstmt = con.prepareStatement(query);
+
+    //Récupération des données saisies par l'utilisateur
+                        int id = Integer.parseInt(idfield.getText());
+                        String name = namefield.getText();
+
+    //Remplissage des paramètres de la requete SQL
+                        pstmt.setInt(1, id);
+                        pstmt.setString(2, name);
+
+    // Exécution de la requete SQL
+                        int rowsAffected = pstmt.executeUpdate();
+
+    //Fermeture de la connexion et du PreparedStatement
+                        pstmt.close();
+                        con.close();
+
+    //Affichage d'un message de succès ou d'erreur
+                        JOptionPane.showMessageDialog(this, "Ventes ajouté avec succès");
+
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                        JOptionPane.showMessageDialog(this, "Erreur lors de l'ajout de la ventes" + ex.getMessage());
+                    }
+
+                }
+    }
+
